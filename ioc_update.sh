04559327a67c0563adaa4fb6232c64af592bfade
Naveen-Kumar <30574883+navein-kumar@users.noreply.github.com>
2024-07-07 23:22:09 +0530

    Create ioc_update.sh

---
+++ ioc_update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/tif.medium-onlydomains.txt"  -o domains.txt &&
+sed '/^#/d' domains.txt > domains1.txt &&
+while IFS= read -r line; do echo -n $line | base64 -w 1000; done < domains1.txt > domains1_iocs.list &&
+rm domains.txt domains1.txt
