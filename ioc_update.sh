aa49f8718b2495f48f2b7fac8bea830ffe90cc9d
Naveen-Kumar <30574883+navein-kumar@users.noreply.github.com>
2024-07-08 01:26:57 +0530

    Update ioc_update.sh

--- ioc_update.sh
+++ ioc_update.sh
@@ -6,11 +6,12 @@ sort merge1.txt | uniq > final1.txt &&
 sed '/^#/d' final1.txt > domains1.txt &&
 while IFS= read -r line; do echo -n $line | base64 -w 1000; done < domains1.txt > domains_iocs1.list &&
 rm 1.txt 2.txt merge1.txt final1.txt  &&
-
+echo "50% done" &&
 curl "https://raw.githubusercontent.com/xRuffKez/NRD/main/nrd-30day_part1.txt"  -o 1.txt &&
 curl "https://raw.githubusercontent.com/xRuffKez/NRD/main/nrd-30day_part2.txt"  -o 2.txt &&
 cat 1.txt 2.txt domains1 > merge2.txt &&
 sort merge2.txt | uniq > final2.txt &&
 sed '/^#/d' final2.txt > domains2.txt &&
 while IFS= read -r line; do echo -n $line | base64 -w 1000; done < domains2.txt > domains_iocs2.list &&
-rm  1.txt 2.txt domains1.txt domains2.txt final2.txt merge2.txt
+rm  1.txt 2.txt domains1.txt domains2.txt final2.txt merge2.txt &&
+echo "100% done" 
