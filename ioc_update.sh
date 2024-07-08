433b91196b1d427833aed6c95787e1790d5800b7
Naveen-Kumar <30574883+navein-kumar@users.noreply.github.com>
2024-07-08 19:36:54 +0530

    Update ioc_update.sh

--- ioc_update.sh
+++ ioc_update.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
-curl "https://raw.githubusercontent.com/stamparm/blackbook/master/blackbook.txt" -o 1.txt ;
+curl "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/tif-onlydomains.txt" -o 1.txt ;
 #curl "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/tif.medium.txt" -o 2.txt ;
 #cat 1.txt 2.txt > merge1.txt ;
 echo "merge  done" ;
 sort 1.txt | uniq > final1.txt ;
 sed '/^#/d' final1.txt > domains1.txt ;
 echo "base64 1 start" ;
+> domains_iocs1.list ;
 while IFS= read -r line; do echo -n $line | base64 -w 1000; done < domains1.txt > domains_iocs1.list ;
 rm 1.txt final1.txt  ;
 echo "50% done" ;
 #curl "https://raw.githubusercontent.com/xRuffKez/NRD/main/nrd-14day.txt"  -o 1.txt ;
-curl "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/tif.medium-onlydomains.txt"  -o 2.txt ;
+curl "https://raw.githubusercontent.com/xRuffKez/NRD/main/nrd-14day.txt"  -o 2.txt ;
 cat  2.txt domains1.txt > merge2.txt ;
 sort merge2.txt | uniq > final2.txt ;
 echo "merge 2 done" ;
 sed '/^#/d' final2.txt > domains2.txt ;
 echo "base64 2 start" ;
+> domains_iocs1.list ;
 while IFS= read -r line; do echo -n $line | base64 -w 1000; done < domains2.txt > domains_iocs2.list ;
 echo "base64 2 done" ;
 rm  1.txt 2.txt domains1.txt domains2.txt final2.txt merge2.txt ;
 echo "100% done" ;
 chown --reference=suricata.yaml domains_iocs2.list domains_iocs1.list ; 
 docker restart suricata
-#docker exec -it --user suricata suricata bash \ cd /etc/suricata/ \ cp domains_iocs2.list domains_iocs1.list ./rules \
 exit
