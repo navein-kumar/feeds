78205610bbc86f8712312d22e66743eeffcfc5bc
Naveen-Kumar <30574883+navein-kumar@users.noreply.github.com>
2024-07-08 14:13:51 +0530

    Update ioc_update.sh

--- ioc_update.sh
+++ ioc_update.sh
@@ -2,8 +2,10 @@
 curl "https://raw.githubusercontent.com/stamparm/blackbook/master/blackbook.txt" -o 1.txt &&
 curl "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/tif.txt" -o 2.txt &&
 cat 1.txt 2.txt > merge1.txt &&
+echo "merge  done" &&
 sort merge1.txt | uniq > final1.txt &&
 sed '/^#/d' final1.txt > domains1.txt &&
+echo "base64 1 start" &&
 while IFS= read -r line; do echo -n $line | base64 -w 1000; done < domains1.txt > domains_iocs1.list &&
 rm 1.txt 2.txt merge1.txt final1.txt  &&
 echo "50% done" &&
@@ -11,7 +13,12 @@ curl "https://raw.githubusercontent.com/xRuffKez/NRD/main/nrd-30day_part1.txt"  
 curl "https://raw.githubusercontent.com/xRuffKez/NRD/main/nrd-30day_part2.txt"  -o 2.txt &&
 cat 1.txt 2.txt domains1 > merge2.txt &&
 sort merge2.txt | uniq > final2.txt &&
+echo "merge 2 done" &&
 sed '/^#/d' final2.txt > domains2.txt &&
 while IFS= read -r line; do echo -n $line | base64 -w 1000; done < domains2.txt > domains_iocs2.list &&
+echo "base64 2 start" &&
 rm  1.txt 2.txt domains1.txt domains2.txt final2.txt merge2.txt &&
 echo "100% done" 
+chown --reference=suricata.yaml domains_iocs2.list domains_iocs1.list &&
+docker exec -it --user suricata suricata bash \ cd /etc/suricata/ \ cp domains_iocs2.list domains_iocs1.list ./rules \
+exit
